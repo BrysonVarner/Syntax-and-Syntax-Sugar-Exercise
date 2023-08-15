2c7fc5206f47d21f3407702bba869f069fb7d8c7
Bryson Varner <brysonandmellisa@hotmail.com>
2023-08-15 15:24:32 -0500

    Completed Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //inferred typing
+            var answer = 4;
+            // string interpolation and ternary operation conversion
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine.";
+            Console.WriteLine(response);    
+
         }
     }
 }
